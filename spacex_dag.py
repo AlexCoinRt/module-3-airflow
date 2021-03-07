b96bfb51fa6dc98240154e794e161d69b7620f46
AlexCoinRt <64779334+AlexCoinRt@users.noreply.github.com>
2021-03-07 23:22:33 +0300

    Update spacex_dag.py

--- spacex_dag.py
+++ spacex_dag.py
@@ -13,22 +13,22 @@ default_args = {
     "retry_delay": timedelta(minutes=5)
 }
 
-rocket_type = ["all", "falcon1", "falcon9", "falconheavy"]
-for i in rocket_type:
+#rocket_type = ["all", "falcon1", "falcon9", "falconheavy"]
+#for i in rocket_type:
     
-    dag = DAG("spacex", default_args=default_args, schedule_interval="0 0 1 1 *")
+dag = DAG("spacex", default_args=default_args, schedule_interval="0 0 1 1 *")
 
-    t1 = BashOperator(
-        task_id="get_data_"+i, 
-        bash_command="python3 /root/airflow/dags/spacex/load_launches.py -y {{ execution_date.year }} -o /var/data", 
-        dag=dag
-    )
+t1 = BashOperator(
+    task_id="get_data_", 
+    bash_command="python3 /root/airflow/dags/spacex/load_launches.py -y {{ execution_date.year }} -o /var/data", 
+    dag=dag
+)
 
-    t2 = BashOperator(
-        task_id="print_data_"+i, 
-        bash_command="cat /var/data/year={{ execution_date.year }}/rocket={{ params.rocket }}/data.csv", 
-        params={"rocket": i}, # falcon1/falcon9/falconheavy
-        dag=dag
-    )
+t2 = BashOperator(
+    task_id="print_data_", 
+    bash_command="cat /var/data/year={{ execution_date.year }}/rocket={{ params.rocket }}/data.csv", 
+    params={"rocket": "all"}, # falcon1/falcon9/falconheavy
+    dag=dag
+)
 
-    t1 >> t2
+t1 >> t2
